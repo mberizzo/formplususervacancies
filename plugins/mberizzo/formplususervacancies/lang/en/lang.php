e680f9c6553b9b2dc67fd332be20282592e9b6e6
Matias Berizzo <matiasberizzo@gmail.com>
2019-07-11 07:43:47 -0300

    Add translations

--- plugins/mberizzo/formplususervacancies/lang/en/lang.php
+++ plugins/mberizzo/formplususervacancies/lang/en/lang.php
@@ -6,10 +6,28 @@ return [
         'description' => 'Vacancies Description',
     ],
     'labels' => [
+        'id' => 'ID',
         'title' => 'Title',
+        'name' => 'Name',
         'description' => 'Description',
+        'categories' => 'Categories',
+        'category' => 'Category',
+        'tags' => 'Tags',
+        'image' => 'Image',
+        'date' => 'Date',
+        'actions' => 'Actions',
     ],
     'settings' => [
         'job_id' => 'Job Identifier',
+    ],
+    'titles' => [
+        'category' => 'Category',
+        'categories' => 'Categories',
+        'user_list' => 'User list',
+        'jobs' => 'Jobs',
+        'job' => 'Job',
+    ],
+    'misc' => [
+        'more_data' => 'View more',
     ],
 ];
---
+++ plugins/mberizzo/formplususervacancies/lang/es/lang.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Vacantes',
+        'description' => 'Administre ofertas laborales.',
+    ],
+    'labels' => [
+        'id' => 'ID',
+        'title' => 'Título',
+        'name' => 'Nombre',
+        'description' => 'Descripción',
+        'categories' => 'Categorías',
+        'category' => 'Categoría',
+        'tags' => 'Tags',
+        'image' => 'Imagen',
+        'date' => 'Fecha',
+        'actions' => 'Acciones',
+    ],
+    'settings' => [
+        'job_id' => 'Vacante ID',
+    ],
+    'titles' => [
+        'category' => 'Categoría',
+        'categories' => 'Categorías',
+        'user_list' => 'Postulantes',
+        'jobs' => 'Vacantes',
+        'job' => 'Vacante',
+    ],
+    'misc' => [
+        'more_data' => 'Ver mas',
+    ],
+];
